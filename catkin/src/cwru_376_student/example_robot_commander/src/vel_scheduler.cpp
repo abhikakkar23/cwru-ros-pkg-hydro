b4c65d23872ff551cc2299a6ea0bbad7dae52f7a
axk596 <axk596@wyatt-OptiPlex-9020.(none)>
2015-02-04 14:17:02 -0500

    copied over files from previous directory.

--- catkin/src/cwru_376_student/example_robot_commander/src/vel_scheduler.cpp
+++ catkin/src/cwru_376_student/example_robot_commander/src/vel_scheduler.cpp
@@ -98,7 +98,7 @@ int main(int argc, char **argv) {
 
     // here is a crude description of one segment of a journey.  Will want to generalize this to handle multiple segments
     // define the desired path length of this segment
-    double segment_length = 0.25; // desired travel distance in meters; anticipate travelling multiple segments
+    double segment_length = 5.00; // desired travel distance in meters; anticipate travelling multiple segments
     
     //here's a subtlety:  might be tempted to measure distance to the goal, instead of distance from the start.
     // HOWEVER, will NEVER satisfy distance to goal = 0 precisely, but WILL eventually move far enought to satisfy distance travelled condition
@@ -202,6 +202,134 @@ int main(int argc, char **argv) {
         // will want to generalize this to handle multiple segments
         // ideally, will want to receive segments dynamically as publications from a higher-level planner
     }
+
+    // Turn 1 
+    start_phi = odom_phi_;
+ while (ros::ok()) // do work here in infinite loop (desired for this example), but terminate if detect ROS has faulted (or ctl-C)
+    {
+        ros::spinOnce(); // allow callbacks to populate fresh data
+        // compute distance travelled so far:
+	
+	segment_length = -1.58; 
+	  segment_length_done= odom_phi_ - start_phi;
+        ROS_INFO("dist travelled: %f", segment_length_done);
+	ROS_INFO("segment length: %f", segment_length);
+	ROS_INFO("odom length : %f", odom_phi_);
+	ROS_INFO("start phi: %f", start_phi);
+        double dist_to_go = segment_length - segment_length_done;
+
+        //use segment_length_done to decide what vel should be, as per plan
+        if (dist_to_go>= 0.0) { // at goal, or overshot; stop!
+            scheduled_vel=0.0;
+        }
+	// std::abs()
+        else if (dist_to_go <= dist_decel) { //possibly should be braking to a halt
+            // dist = 0.5*a*t_halt^2; so t_halt = sqrt(2*dist/a);   v = a*t_halt
+            // so v = a*sqrt(2*dist/a) = sqrt(2*dist*a)
+	  scheduled_vel = -1*sqrt(50 *std::abs( dist_to_go) * a_max);
+            ROS_INFO("braking zone: v_sched = %f",scheduled_vel);
+        }
+        else { // not ready to decel, so target vel is v_max, either accel to it or hold it
+            scheduled_vel = v_max;
+        }
+        
+  
+
+        //how does the current velocity compare to the scheduled vel?
+        if (odom_vel_ < scheduled_vel) {  // maybe we halted, e.g. due to estop or obstacle;
+            // may need to ramp up to v_max; do so within accel limits
+            double v_test = odom_vel_ + a_max*dt_callback_; // if callbacks are slow, this could be abrupt
+            // operator:  c = (a>b) ? a : b;
+            new_cmd_omega = (v_test < scheduled_vel) ? v_test : scheduled_vel; //choose lesser of two options
+            // this prevents overshooting scheduled_vel
+        } else if (odom_vel_ > scheduled_vel) { //travelling too fast--this could be trouble
+            // ramp down to the scheduled velocity.  However, scheduled velocity might already be ramping down at a_max.
+            // need to catch up, so ramp down even faster than a_max.  Try 1.2*a_max.
+            ROS_INFO("odom vel: %f; sched vel: %f",odom_vel_,scheduled_vel); //debug/analysis output; can comment this out
+            
+            double v_test = odom_vel_ - 1.2 * a_max*dt_callback_; //moving too fast--try decelerating faster than nominal a_max
+
+            new_cmd_omega = (v_test > scheduled_vel) ? v_test : scheduled_vel; // choose larger of two options...don't overshoot scheduled_vel
+        } else {
+            new_cmd_omega = scheduled_vel; //silly third case: this is already true, if here.  Issue the scheduled velocity
+        }
+        ROS_INFO("cmd vel: %f",new_cmd_omega); // debug output
+
+        cmd_vel.linear.x = 0.0;
+        cmd_vel.angular.z = new_cmd_omega; // spin command; always zero, in this example
+        if (dist_to_go>= 0.0) { //uh-oh...went too far already!
+            cmd_vel.linear.x = 0.0;
+	    cmd_vel.angular.z = 0.0;//command vel=0
+        }
+        vel_cmd_publisher.publish(cmd_vel); // publish the command to robot0/cmd_vel
+        rtimer.sleep(); // sleep for remainder of timed iteration
+        if (dist_to_go>= 0.0) break; // halt this node when this segment is complete.
+        // will want to generalize this to handle multiple segments
+        // ideally, will want to receive segments dynamically as publications from a higher-level planner
+    }
+
+ //Go Straight after first turn
+ 
+ double segment_length = 15.00;
+ while (ros::ok()) // do work here in infinite loop (desired for this example), but terminate if detect ROS has faulted (or ctl-C)
+    {
+        ros::spinOnce(); // allow callbacks to populate fresh data
+        // compute distance travelled so far:
+        double delta_x = odom_x_ - start_x;
+        double delta_y = odom_y_ - start_y;
+        segment_length_done = sqrt(delta_x * delta_x + delta_y * delta_y);
+        ROS_INFO("dist travelled: %f", segment_length_done);
+        double dist_to_go = segment_length - segment_length_done;
+
+        //use segment_length_done to decide what vel should be, as per plan
+        if (dist_to_go<= 0.0) { // at goal, or overshot; stop!
+            scheduled_vel=0.0;
+        }
+        else if (dist_to_go <= dist_decel) { //possibly should be braking to a halt
+            // dist = 0.5*a*t_halt^2; so t_halt = sqrt(2*dist/a);   v = a*t_halt
+            // so v = a*sqrt(2*dist/a) = sqrt(2*dist*a)
+            scheduled_vel = sqrt(2 * dist_to_go * a_max);
+            ROS_INFO("braking zone: v_sched = %f",scheduled_vel);
+        }
+        else { // not ready to decel, so target vel is v_max, either accel to it or hold it
+            scheduled_vel = v_max;
+        }
+        
+  
+
+        //how does the current velocity compare to the scheduled vel?
+        if (odom_vel_ < scheduled_vel) {  // maybe we halted, e.g. due to estop or obstacle;
+            // may need to ramp up to v_max; do so within accel limits
+            double v_test = odom_vel_ + a_max*dt_callback_; // if callbacks are slow, this could be abrupt
+            // operator:  c = (a>b) ? a : b;
+            new_cmd_vel = (v_test < scheduled_vel) ? v_test : scheduled_vel; //choose lesser of two options
+            // this prevents overshooting scheduled_vel
+        } else if (odom_vel_ > scheduled_vel) { //travelling too fast--this could be trouble
+            // ramp down to the scheduled velocity.  However, scheduled velocity might already be ramping down at a_max.
+            // need to catch up, so ramp down even faster than a_max.  Try 1.2*a_max.
+            ROS_INFO("odom vel: %f; sched vel: %f",odom_vel_,scheduled_vel); //debug/analysis output; can comment this out
+            
+            double v_test = odom_vel_ - 1.2 * a_max*dt_callback_; //moving too fast--try decelerating faster than nominal a_max
+
+            new_cmd_vel = (v_test > scheduled_vel) ? v_test : scheduled_vel; // choose larger of two options...don't overshoot scheduled_vel
+        } else {
+            new_cmd_vel = scheduled_vel; //silly third case: this is already true, if here.  Issue the scheduled velocity
+        }
+        ROS_INFO("cmd vel: %f",new_cmd_vel); // debug output
+
+        cmd_vel.linear.x = new_cmd_vel;
+        cmd_vel.angular.z = new_cmd_omega; // spin command; always zero, in this example
+        if (dist_to_go <= 0.0) { //uh-oh...went too far already!
+            cmd_vel.linear.x = 0.0;  //command vel=0
+        }
+        vel_cmd_publisher.publish(cmd_vel); // publish the command to robot0/cmd_vel
+        rtimer.sleep(); // sleep for remainder of timed iteration
+        if (dist_to_go <= 0.0) break; // halt this node when this segment is complete.
+        // will want to generalize this to handle multiple segments
+        // ideally, will want to receive segments dynamically as publications from a higher-level planner
+    }
+
+
     ROS_INFO("completed move distance");
 }
 
